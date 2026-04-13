05c62bdf6c5af8f9ed97c409b57c7eb7dc5841fa
Anton Shaga <antonyshaga@gmail.com>
2026-04-13 09:10:37 -0300

    feat: production setup (nginx, docker, CI/CD) and remove .idea

--- frontend/src/shared/lib/socket/socket.ts
+++ frontend/src/shared/lib/socket/socket.ts
@@ -1,14 +1,13 @@
 import { io, Socket } from 'socket.io-client'
 
-const WS_URL = process.env.NEXT_PUBLIC_WS_URL!
-
 declare global {
   var __SOCKET__: Socket | undefined
 }
 
 export const getSocket = (): Socket => {
   if (!globalThis.__SOCKET__) {
-    globalThis.__SOCKET__ = io(WS_URL, {
+    globalThis.__SOCKET__ = io({
+      path: '/socket.io',
       withCredentials: true,
     })
   }
