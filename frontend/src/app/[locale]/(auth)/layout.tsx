d00a087ab1e2d23b4f3dd12f520151489b949ec9
Anton Shaga <antonyshaga@gmail.com>
2026-04-13 22:37:31 -0300

    refactor(auth): move auth logic to layout, remove hydration

--- frontend/src/app/[locale]/(auth)/layout.tsx
+++ frontend/src/app/[locale]/(auth)/layout.tsx
@@ -1,3 +1,20 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+import React from 'react'
+import { serverFetch } from '@/shared/api/server'
+import { redirect } from 'next/navigation'
+
+export default async function AuthLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
+}) {
+  const { locale } = await params
+
+  const orders = await serverFetch('/orders')
+
+  if (orders.data) {
+    redirect(`/${locale}/orders`)
+  }
   return <div className="flex items-center justify-center min-h-screen">{children}</div>
 }
